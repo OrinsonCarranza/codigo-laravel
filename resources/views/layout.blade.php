d58cd2f1e8dfbb306d1cb1520d0752a7ce826108
OrinsonCarranza <orinsoncarranza@gmail.com>
2024-05-31 23:47:10 -0500

    Lab semana 04 - Codigo-Laravel

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <!-- <h1>Home</h1> -->
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="nosotros">Nosotros</a></li>
+        <li><a href="servicios">Servicios</a></li>
+        <li><a href="contacto">Contacto</a></li>
+    </ul>
+    @yield('content')
+</body>
+</html>
